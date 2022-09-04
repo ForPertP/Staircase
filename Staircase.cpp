f134f8d6b7ed57ec15fdce3c4bc1d36f478eab72
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-09-04 10:47:07 +0900

    Update Staircase.cpp

--- Staircase.cpp
+++ Staircase.cpp
@@ -1,3 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
 void staircase(int n)
 {
     for (int i = 1; i <= n; ++i)
@@ -5,4 +18,16 @@ void staircase(int n)
         std::cout << std::right << std::setfill(' ')
             << std::setw(n) << std::string(i,'#') << std::endl;
     }
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(n_temp);
+
+    staircase(n);
+
+    return 0;
 }
